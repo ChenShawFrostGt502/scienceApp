6907acd4ea62bc312e0bafd21b567cb2e4caa342
Raxel21 <yisusparley@gmail.com>
2019-04-03 23:53:17 -0600

    Version 04.04.19

--- www/js/app.js
+++ www/js/app.js
@@ -81,7 +81,17 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'AccountCtrl'
       }
     }
-  });
+  })
+  .state('register', {
+    url: '/register',
+    templateUrl: 'templates/register.html',
+    controller: 'RegisterCtrl'
+  })
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
