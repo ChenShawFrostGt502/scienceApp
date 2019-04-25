52416eeb55936b29be75f7ee11c1b7aff1b5e67d
Raxel21 <yisusparley@gmail.com>
2019-04-24 21:44:03 -0600

    Version 24.04.19

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,6 +9,23 @@
   };
   firebase.initializeApp(config);
 
+  firebase.firestore().enablePersistence()
+  .then(function() {
+      // Initialize Cloud Firestore through firebase
+      var db = firebase.firestore();
+  })
+  .catch(function(err) {
+      if (err.code == 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          // ...
+      } else if (err.code == 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence
+          // ...
+      }
+  });
+
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, $state) {
@@ -85,7 +102,7 @@ angular.module('starter.controllers', [])
       // to leave the inputs  empty  
       $scope.user = {};
       // if the credentials are correct redirect to home
-      $state.go('tab.dash');
+      $state.go('home');
     })
     // In case of any error you will tell us in a message indicating the same
     .catch(function(error) {
@@ -93,7 +110,7 @@ angular.module('starter.controllers', [])
       var errorCode = error.code;
       var errorMessage = error.message;
       // ...
-      console.log(error.message);
+      alert(error.message);
     });
   };
 
@@ -108,45 +125,124 @@ angular.module('starter.controllers', [])
         //to leave the inputs  empty
         $scope.user = {};
         // if the credentials are correct redirect to login
-        $state.go('tab.dash');
+        $state.go('home');
       })
       .catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
-        console.log(error.message)
+        alert(error.message)
       });
     };
   };
   $scope.goregister = function() {
-    $state.go('register')
+    $state.go('register');
+  };
+  $scope.goHome = function() {
+    $state.go('home');
   };
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+// Controller for Home
+.controller('homeCtrl', function($scope, $state){
+  $scope.DNA = function() {
+    $state.go('DNA');
+  }
+  $scope.Videoi = function() {
+    $state.go('videoi')
+  }
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('DNACtrl', function($scope) {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  const { width, height } = canvas;
+  const xCenter = width / 2; // centre de l'écran en x
+  const yStart = 50; // positionde la première hélice en y
+  const xMax = 80; // largeur des helices
+  const yMax = 50; // espace entre les helices
+  let angle = 0;
+  const speed = 0.02; // vitesse de rotation
+  const tAnglePI = [
+  0,
+  Math.PI / 6,
+  Math.PI / 3,
+  Math.PI / 2,
+  2 * Math.PI / 3,
+  5 * Math.PI / 6,
+  Math.PI,
+  7 * Math.PI / 6,
+  4 * Math.PI / 3,
+  -Math.PI / 2,
+  5 * Math.PI / 3,
+  11 * Math.PI / 6,
+  ];
+  const tPI = [...tAnglePI, ...tAnglePI];
+  function render() {
+    ctx.clearRect(0, 0, width, height);
+    const tPositions = [];
+    for (let i = 0; i < tPI.length; i++) {
+      const positions = adn(tPI[i], (i * yMax));
+      tPositions.push(positions);
+    }
+    angle += speed;
+    requestAnimationFrame(render);
+  }
+  /**
+  *
+  * @param {*} startPhase phase de début en x
+  * @param {*} yPos position de l'helice en y
+  */
+  function adn(startPhase, yPos, colorLink = '#FFFFFF') {
+    const angleSin = Math.sin(angle + startPhase);
+    const angleCos = Math.cos(angle + startPhase);
+    const xPos1 = angleSin * xMax;
+    const xPos2 = -angleSin * xMax;
+    const yPos1 = angleCos * 10 + yPos;
+    const yPos2 = -angleCos * 10 + yPos;
+    // Création des liens entre les cercles
+    ctx.beginPath();
+    ctx.strokeStyle = colorLink;
+    ctx.lineWidth = 3;
+    ctx.moveTo(xPos1 + xCenter, yPos1 + yStart);
+    ctx.lineTo(xPos2 + xCenter, yPos2 + yStart);
+    ctx.stroke();
+    // On fait passer le cercle le plus loin derrière
+    if (angleCos < 0) {
+      drawADN(xPos1, yPos1, angleCos, 11342935);
+      drawADN(xPos2, yPos2, -angleCos, 1668818);
+    } else {
+      drawADN(xPos2, yPos2, -angleCos, 1668818);
+      drawADN(xPos1, yPos1, angleCos, 11342935);
+    }
+    return {
+      xPos1, yPos1, xPos2, yPos2,
+    };
+  }
+  /**
+  * @param {*} xAngle position en x du cercle
+  * @param {*} yAngle position en y du cercle
+  * @param {*} radius largeur du cercle
+  * @param {*} color couleur du cercle
+  */
+  function drawADN(xPos, yPos, radius, color) {
+    ctx.fillStyle = `#${(color).toString(16)}`;
+    ctx.beginPath();
+    ctx.arc(
+      xPos + xCenter,
+      yPos + yStart,
+      10 + (radius * 3),
+      0,  
+      2 * Math.PI,
+    );
+    ctx.closePath();
+    ctx.fill();
+    }
+    render();
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
-
-
+.controller('VideoCtrl', function($scope) {
+})
