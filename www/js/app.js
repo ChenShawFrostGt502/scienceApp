b395300809f0e313fd3dc569b5fa3e958e75e1e3
Raxel21 <yisusparley@gmail.com>
2019-04-30 22:13:41 -0600

    Version 30.04.19

--- www/js/app.js
+++ www/js/app.js
@@ -77,5 +77,3 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/login');
 });
-
-
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -49,6 +49,16 @@ angular.module('starter.controllers', [])
       var errorCode = error.code;
       var errorMessage = error.message;
       // ...
+      // alert(errorMessage);
+      if (errorMessage == "The email address is badly formatted.") {
+        alert("El formato de email no es válido.");
+      };
+      if (errorMessage == "Password should be at least 6 characters") {
+        alert("La contraseña debe tener al menos 6 caracteres.");
+      };
+      if (errorMessage == "The email address is already in use by another account.") {
+        alert("La dirección de correo electrónico ya está en uso por otra cuenta.");
+      };
     });
   };
 
@@ -73,15 +83,26 @@ angular.module('starter.controllers', [])
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        alert(error.message);
+        // alert(error.message);
         // ...
+        if (errorMessage == "The email address is badly formatted.") {
+          alert("El formato de email no es válido.");
+        };
+        if (errorMessage == "Password should be at least 6 characters") {
+          alert("La contraseña debe tener al menos 6 caracteres.");
+        };
+        if (errorMessage == "The email address is already in use by another account.") {
+          alert("La dirección de correo electrónico ya está en uso por otra cuenta.");
+        };
       });
     };
   };
-
   $scope.gologin = function () {
     // redirect to login   
-    $state.go('login')
+    $state.go('login');
+  };
+  $scope.goHome = function() {
+    $state.go('home');
   };
 })
 
@@ -102,9 +123,17 @@ angular.module('starter.controllers', [])
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      // ...
+      // alert(errorMessage);
+      console.log(errorMessage);
+      // Messages to show before possible errors
       if (errorMessage == "The password is invalid or the user does not have a password.") {
         alert("El usuario o contraseña son incorrectos.");  
+      };
+      if (errorMessage == "There is no user record corresponding to this identifier. The user may have been deleted.") {
+        alert("No hay registro de usuario correspondiente a este email. El usuario puede haber sido eliminado.");
+      };
+      if (errorMessage == "The email address is badly formatted.") {
+        alert("El formato de email no es válido.");
       };
     });
   };
@@ -126,18 +155,23 @@ angular.module('starter.controllers', [])
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
+        // alert(error.message);
+        console.log(errorMessage);
         // ...
         if (errorMessage == "The password is invalid or the user does not have a password.") {
           alert("El usuario o contraseña son incorrectos.");  
+        };
+        if (errorMessage == "There is no user record corresponding to this identifier. The user may have been deleted.") {
+          alert("No hay registro de usuario correspondiente a este email. El usuario puede haber sido eliminado.");
+        };
+        if (errorMessage == "The email address is badly formatted.") {
+          alert("El formato de email no es válido.");
         };
       });
     };
   };
   $scope.goregister = function() {
     $state.go('register');
-  };
-  $scope.goHome = function() {
-    $state.go('home');
   };
 })
 
@@ -153,10 +187,10 @@ angular.module('starter.controllers', [])
     $state.go('tNutri');
   };
   $scope.info = function() {
-    $state.go('infoG')
+    $state.go('infoG');
   };
   $scope.juego = function() {
-    $state.go('juego')
+    $state.go('juego');
   };
 })
 
@@ -264,4 +298,150 @@ angular.module('starter.controllers', [])
 
 // Controller for state juego
 .controller('juegoCtrl', function($scope) {
-})
+  /*$scope.ancho = function() {
+    $scope.medida =  window.innerWidth;
+    alert($scope.medida);
+  };*/
+  /*var symbols = ['bicycle', 'bicycle', 'leaf', 'leaf', 'cube', 'cube', 'anchor', 'anchor', 'paper-plane-o', 'paper-plane-o', 'bolt', 'bolt', 'bomb', 'bomb', 'diamond', 'diamond'],*/  
+  var palabras = ['Vit A', 'Vit A', 'Vit B1', 'Vit B1', 'Vit C', 'Vit C', 'Vit D', 'Vit D', 'Vit B2', 'Vit B2', 'Vit E', 'Vit E', 'Vit B12', 'Vit B12', 'Vit K1', 'Vit K1'],
+    opened = [],
+    match = 0,
+    moves = 0,
+    $deck = $('.deck'),
+    $scorePanel = $('#score-panel'),
+    $moveNum = $scorePanel.find('.moves'),
+    $ratingStars = $scorePanel.find('i'),
+    $restart = $scorePanel.find('.restart'),
+    delay = 800,
+    gameCardsQTY = palabras.length / 2,
+    rank3stars = gameCardsQTY + 2,
+    rank2stars = gameCardsQTY + 6,
+    rank1stars = gameCardsQTY + 10;
+
+// Shuffle function From http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// Initial Game
+function initGame() {
+  var cards = shuffle(palabras);
+  $deck.empty();
+  match = 0;
+  moves = 0;
+  $moveNum.html(moves);
+  $ratingStars.removeClass('fa-star-o').addClass('fa-star');
+  for (var i = 0; i < cards.length; i++) {
+    // $deck.append($('<li class="card"><i class="fa fa-' + cards[i] + '"></i></li>'))
+    $deck.append($('<li class="card">' + cards[i] + '</li>'))
+  }
+};
+
+// Set Rating and final Score
+function setRating(moves) {
+  var rating = 3;
+  if (moves > rank3stars && moves < rank2stars) {
+    $ratingStars.eq(2).removeClass('fa-star').addClass('fa-star-o');
+    rating = 2;
+  } else if (moves > rank2stars && moves < rank1stars) {
+    $ratingStars.eq(1).removeClass('fa-star').addClass('fa-star-o');
+    rating = 1;
+  } else if (moves > rank1stars) {
+    $ratingStars.eq(0).removeClass('fa-star').addClass('fa-star-o');
+    rating = 0;
+  } 
+  return { score: rating };
+};
+
+// End Game
+function endGame(moves, score) {
+  swal({
+    allowEscapeKey: false,
+    allowOutsideClick: false,
+    title: '¡Felicitaciones! ¡Ganaste!',
+    text: 'Con ' + moves + ' Movimientos y ' + score + ' Estrellas.\n!Boom Shaka Lak!',
+    type: 'success',
+    showCancelButton: true,
+    confirmButtonColor: '#9BCB3C',
+    cancelButtonColor: '#EE0E51',
+    confirmButtonText: 'Jugar de nuevo!'
+  }).then(function(isConfirm) {
+    if (isConfirm) {
+      initGame();
+    }
+  })
+}
+
+// Restart Game
+$restart.on('click', function() {
+  swal({
+    allowEscapeKey: false,
+    allowOutsideClick: false,
+    title: '¿Estás seguro?',
+    text: "¡Tu progreso se perderá!",
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#9BCB3C',
+    cancelButtonColor: '#EE0E51',
+    confirmButtonText: '¡Sí, Reiniciar Juego!'
+  }).then(function(isConfirm) {
+    if (isConfirm) {
+      initGame();
+    }
+  })
+});
+
+// Card flip
+$deck.on('click', '.card:not(".match, .open")', function() {
+  if($('.show').length > 1) { return true; }
+  
+  var $this = $(this),
+      card = $this.context.innerHTML;
+  $this.addClass('open show');
+  opened.push(card);
+
+  // Compare with opened card
+  if (opened.length > 1) {
+    if (card === opened[0]) {
+      $deck.find('.open').addClass('match animated infinite rubberBand');
+      setTimeout(function() {
+        $deck.find('.match').removeClass('open show animated infinite rubberBand');
+      }, delay);
+      match++;
+    } else {
+      $deck.find('.open').addClass('notmatch animated infinite wobble');
+      setTimeout(function() {
+        $deck.find('.open').removeClass('animated infinite wobble');
+      }, delay / 1.5);
+      setTimeout(function() {
+        $deck.find('.open').removeClass('open show notmatch animated infinite wobble');
+      }, delay);
+    }
+    opened = [];
+    moves++;
+    setRating(moves);
+    $moveNum.html(moves);
+  }
+  
+  // End Game if match all cards
+  if (gameCardsQTY === match) {
+    setRating(moves);
+    var score = setRating(moves).score;
+    setTimeout(function() {
+      endGame(moves, score);
+    }, 500);
+  }
+});
+
+initGame();
+});
